aebc739219fcfeb00ae021d3b8006d7dc0688557
Asiphe04 <asiphendimlana40@gmail.com>
2024-01-11 10:08:19 +0200

    limked to progressql

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import '@picocss/pico'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+       <body >
+      <main className='container'>
+     {children}
+      </main>
+      </body>
     </html>
   )
 }
+
